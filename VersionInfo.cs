60dd0af3b88a64cbd2a26268af2ad3ba3d60900b
Davinci Jeremie <Davincij@gmail.com>
2021-09-08 10:46:39 -0300

    Pandora's Wallet V1.1.47.2617 Release

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -24,5 +24,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.45.2615")]
-[assembly: AssemblyFileVersion("1.1.45.2615")]
+[assembly: AssemblyVersion("1.1.47.2617")]
+[assembly: AssemblyFileVersion("1.1.47.2617")]
